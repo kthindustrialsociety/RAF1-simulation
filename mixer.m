b901729c7fddae89f9e7b46731a068e5f96a0f84
AnderssonL <fabian.andersson2005@gmail.com>
2025-11-07 19:20:15 +1030

    simplified simu using areas and goto's

--- mixer.m
+++ mixer.m
@@ -1,18 +1,19 @@
-function [F, M] = mixer(x)
+function x = mixer(F, M)
 % Converts motor velocities to physical forces and moments
 % See https://wilselby.com/research/arducopter/controller-design/ for info
 
+K_T = 0.1; % got to change
+K_D = 0.01; % got to change
 
-A = [ K_T,  K_T,   K_T,   K_T;
-  0, 0, 0, 0;
+y = [F, M];
+
+A = [ 0, 0, 0, 0;
       0, 0, 0, 0;
+      K_T,  K_T,   K_T,   K_T;
       a*K_T, -a*K_T,  a*K_T, -a*K_T;
      -b*K_T, -b*K_T,  b*K_T,  b*K_T;
      -K_D,   K_D,   -K_D,   K_D ];
 
-y = A*x;
-
-F = y(1:3, :);
-M = y(4:6, :);
+x = A\y;
 
 end
--- reverse_mixer.m
+++ reverse_mixer.m
@@ -1,15 +1,20 @@
-function [M, F] = mixer(x)
+function [F, M] = mixer(x)
 % Converts motor velocities to physical forces and moments
 % See https://wilselby.com/research/arducopter/controller-design/ for info
 
+K_T = 0.1; % got to change
+K_D = 0.01; % got to change
 
-A = [ K_T,  K_T,   K_T,   K_T;
+A = [ 0, 0, 0, 0;
+      0, 0, 0, 0;
+      K_T,  K_T,   K_T,   K_T;
       a*K_T, -a*K_T,  a*K_T, -a*K_T;
      -b*K_T, -b*K_T,  b*K_T,  b*K_T;
      -K_D,   K_D,   -K_D,   K_D ];
 
+y = A*x;
 
-
-
+F = y(1:3, :);
+M = y(4:6, :);
 
 end
